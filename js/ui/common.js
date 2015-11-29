b1dd0eeb4710bfb3ac16ae4720bb831b7a21bd9b
BPanchenko <oberon@list.ru>
2015-11-29 20:01:17 +0300

    refactoring javascript libraries

--- js/ui/common.js
+++ js/ui/common.js
@@ -1,127 +1,32 @@
-if (!Element.prototype.getParentByClassName) {
-	Element.prototype.getParentByClassName = function(className) {
-		var _el = this;
-		do {
-			_el = _el.parentNode;
-			if (_el.classList.contains(className)) return _el;
-		} while (_el.parentNode instanceof Element)
-		return null;
-	}
-}
-if (!Element.prototype.getPreviousByClassName) {
-    Element.prototype.getPreviousByClassName = function (className) {
-        var _el = this;
-        while (_el.previousSibling) {
-            _el = _el.previousSibling;
-            if (_el.classList && _el.classList.contains(className)) return _el;
-        }
-        return null;
-    }
-}
-if (!Element.prototype.getSiblings) {
-    Element.prototype.getSiblings = function () {
-        var _res = [];
-        [].slice.call(this.parentNode.children).forEach(function (__el) {
-            if (this != __el) _res.push(__el);
-        }, this);
-        return _res;
-    }
-}
-if (!Element.prototype.getSiblingsByClassName) {
-    Element.prototype.getSiblingsByClassName = function (className) {
-        var _res = [], _el, _siblings = this.getSiblings(), _i = _siblings.length - 1;
-        for (; _i>=0; _i--) {
-            _el = _siblings[_i];
-            if (_el.classList && _el.classList.contains(className)) _res.push(_el);
-        }
-        return _res;
-    }
-}
 
-if(!Element.prototype.offset) {
-	Element.prototype.offset = function() {
-		var top=0, left=0, el=this;
-		if (el.getBoundingClientRect) {
-			var box = el.getBoundingClientRect(),
-				body = document.body,
-				docElem = document.documentElement,
-				scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop,
-				scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft,
-				clientTop = docElem.clientTop || body.clientTop || 0,
-				clientLeft = docElem.clientLeft || body.clientLeft || 0;
-			
-			top  = Math.round(box.top +  scrollTop - clientTop);
-			left = Math.round(box.left + scrollLeft - clientLeft);
-		} else {
-			while(el) {
-				top = top + parseFloat(el.offsetTop);
-				left = left + parseFloat(el.offsetLeft);
-				el = el.offsetParent;     
-			}
-		}
-		return { 'top': top, 'left': left };
-	}
-}
-
-if (!Element.prototype.remove) {
-    Element.prototype.remove = function () { var p = this.parentElement; p.removeChild(this); return p; }
-}
-
-if(!Element.prototype.width || !Element.prototype.height) {
-	Element.prototype.width = function(value){
-		if(value) {
-			if(/(\drem|em|px|pt)/i.test(value))
-				this.style.width = value;
-			else
-				this.style.width = Math.round(value)+'px';
-			return this;
-		}
-		return this.offsetWidth;
-	}
-	Element.prototype.height = function(value){
-		if(value) {
-			if(/(\drem|em|px|pt)/i.test(value))
-				this.style.height = value;
-			else
-				this.style.height = Math.round(value)+'px';
-			return this;
-		}
-		return this.offsetHeight;
-	}
-}
-
-if (!Number.prototype.ci) {
-    // (1).ci('cущност|ей|ь|и')
+// (1).ci('cущност|ей|ь|и')
+if (!Number.prototype.ci)
     Number.prototype.ci = function (c, f) {
 		f || (f = true);
         c = c.split('|');
         var n = this.toString().substr(-2), res = f ? this + ' ' : '';
         return res + c[0] + ((/^[0,2-9]?[1]$/.test(n)) ? c[2] : ((/^[0,2-9]?[2-4]$/.test(n)) ? c[3] : c[1]));
     }
-}
 
-if (!Number.prototype.pad) {
+if (!Number.prototype.pad)
     Number.prototype.pad = function (size) {
         var s = this + "";
         while (s.length < size) s = "0" + s;
         return s;
     }
-}
 
-if (!Number.prototype.separate) {
+if (!Number.prototype.separate)
 	Number.prototype.separate = function(sep, bits) {
 		var s = sep || '&thinsp;',
 			b = parseInt(bits) || 3,
 			n = this+'';
 		return n.replace(/\d(?=(\d{3})+[^\d])/g, '$&'+sep);
 	}
-}
 
-if (!Number.prototype.toBoolean) {
+if (!Number.prototype.toBoolean)
     Number.prototype.toBoolean = function () {
         return !!+this;
     }
-}
 
 if (!Number.prototype.toWord) {
 	Number.prototype.toWord = function() {
@@ -196,58 +101,7 @@ if (!Number.prototype.toWord) {
 	}
 }
 
-
-document.addEventListener("DOMContentLoaded", function(){
-	
-	[].slice.call(document.body.getElementsByClassName('ui-field-hint')).forEach(function (label) {
-		label.dataset.initvalue = label.innerHTML;
-	});
-	
-	document.body.addEventListener('focus', function(e){
-		var fieldset, label,
-			target = e.target;
-	
-		if(target.classList.contains('ui-field')) {
-			fieldset = target.getParentByClassName('ui-fieldset');
-			label = fieldset.getElementsByClassName('ui-field-hint')[0];
-			if(fieldset instanceof HTMLElement) {
-				label.innerHTML = "focus & filled";
-				fieldset.classList.add('ui-fieldset--focus');
-				fieldset.classList.add('ui-fieldset--filled');
-			}
-		}
-		
-		return;
-	}, true);
-	
-	document.body.addEventListener('blur', function(e){
-		var fieldset, label,
-			field = e.target;
-			
-		if(field.classList.contains('ui-field')) {
-			fieldset = field.getParentByClassName('ui-fieldset');
-			label = fieldset.getElementsByClassName('ui-field-hint')[0];
-			if(fieldset instanceof HTMLElement) {
-				fieldset.classList.remove('ui-fieldset--focus');
-				if(!field.value.trim()) {
-					label.innerHTML = label.dataset.initvalue;
-					fieldset.classList.remove('ui-fieldset--filled');
-				} else
-					label.innerHTML = "filled, value: " + field.value.trim();
-			}
-		}
-		
-		return;
-	}, true);
-	
-	[].slice.call(document.body.getElementsByClassName('ui-field')).forEach(function(field){
-		var fieldset;
-		
-		if(field.value.trim()) {
-			fieldset = field.getParentByClassName('ui-fieldset');
-			if(fieldset instanceof HTMLElement)
-				fieldset.classList.add('ui-fieldset--filled');
-		}
-	});
-	
-}, false);
+if (!String.prototype.toBoolean)
+    String.prototype.toBoolean = function () {
+        return this.toLowerCase() == 'true';
+    }
