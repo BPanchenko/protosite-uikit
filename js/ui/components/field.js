95b7e11acf35550389fc9d78007895a95fb188df
BPanchenko <oberon@list.ru>
2016-02-20 00:40:10 +0300

    processing the search field

--- js/ui/components/field.js
+++ js/ui/components/field.js
@@ -42,8 +42,19 @@
         return;
     };
 
+    var onChangeFieldSearch = _.debounce(function (e) {
+        var $input = $(this),
+            $container = $input.parent('.c-field'),
+            value = $input.val();
+
+        value && $container.removeClass('is-invalid');
+
+        return;
+    }, 150);
+
     var onClickButton = function(e) {
         var $this = $(this),
+            $container = $this.parent('.c-field'),
             $input = $this.siblings('.c-field-input').eq(0),
             $icon = $this.find('.iconic').eq(0);
 
@@ -54,6 +65,12 @@
         } else if($icon.data('glyph') == 'eye-open') {
             $icon.data('glyph', 'eye-closed');
             $input.attr('type', 'password')
+        }
+
+        // processing the search field
+        if($icon.data('glyph') == 'loupe' && !$input.val()) {
+            e.preventDefault();
+            $container.addClass('is-invalid');
         }
 
         return;
@@ -89,8 +106,11 @@
             this.$inputs.filter('*[pattern]:not([type=email])').on('input', checkPattern);
             this.$inputs.filter('*[pattern][type=email]').on('change', checkPattern);
 
-            // processing the field file
+            // processing the file field
             this.$inputs.filter('input[type=file]').on('change', onChangeFieldFile);
+
+            // processing the search field
+            this.$inputs.filter('input[type=search]').on('change input', onChangeFieldSearch);
 
             // click by the field button
             this.$buttons.on('click', onClickButton);
