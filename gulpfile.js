ccb0bc18401b52fe31aa5b2620705c969d0630ea
BPanchenko <oberon@list.ru>
2016-03-26 14:49:32 +0300

    build bundle

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,30 @@
 var gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
 	cssmin = require('gulp-cssmin'),
+    cleanCSS = require('gulp-clean-css'),
     concat = require('gulp-concat'),
-	less = require('gulp-less');
+	less = require('gulp-less'),
+    rename = require('gulp-rename');
 
 gulp.task('css', function () {
-    gulp.src('./css/main.less')
+    gulp.src('./css/core.less')
         .pipe(less())
 		.pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(cssmin())
-        .pipe(gulp.dest('./css/main.min.css'));
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename("core.min.css"))
+        .pipe(gulp.dest('./css/'));
 });
+
+
+var gulp = require('gulp');
+
+gulp.task('minify-css', function() {
+    return gulp.src('styles/*.css')
+        ;
+});
+
 
 gulp.task('build', ['css']);
