7897542a1a24d655cdf471963f2c738dc1d5f990
BPanchenko <oberon@list.ru>
2016-08-19 02:34:09 +0300

    refact ui-core to es2015

--- js/ui/core.js
+++ js/ui/core.js
@@ -3,125 +3,131 @@ var $ = require("jquery"),
     Backbone = require("backbone"),
     moment = require("moment");
 
+(function(){
 
-// DOM-listeners
-var _ons = {
-    'window': {
-        'resize': function(e) {
-            ui.window.set({
-                'width': window.innerWidth,
-                'height': window.innerHeight
-            });
-            return;
+    var _components = [];
+
+    // DOM-listeners
+    var _ons = {
+        'window': {
+            'resize': function(e) {
+                ui.window.set({
+                    'width': window.innerWidth,
+                    'height': window.innerHeight
+                });
+                return;
+            }
         }
-    }
-};
+    };
 
-window.addEventListener('resize', _ons.window.resize, false);
-$(document).ready(_ons.window.resize);
+    // {view} Backdrop
+    var BackdropView = Backbone.View.extend({
+        name: 'BackdropView',
+        tagName: 'div',
+        className: 'c-backdrop',
 
-// {view} Backdrop
-var _backdrop = new (Backbone.View.extend({
-    tagName: 'div',
-    className: 'c-backdrop',
+        events: {
+            'click': function (e) { this.trigger('click', this, e); }
+        },
 
-    events: {
-        'click': function (e) { this.trigger('click', this, e); }
-    },
+        show: function (options) {
+            options || (options = {});
+            document.body.appendChild(this.el);
 
-    show: function (options) {
-        options || (options = {});
-        document.body.appendChild(this.el);
+            _.isNumber(options.opacity) && (this.el.style.opacity = options.opacity);
+            _.isNumber(options.zIndex) && (this.el.style.zIndex = options.zIndex);
 
-        _.isNumber(options.opacity) && (this.el.style.opacity = options.opacity);
-        _.isNumber(options.zIndex) && (this.el.style.zIndex = options.zIndex);
+            this.trigger('show', this);
+            return this;
+        },
+        hide: function () {
+            this.el.style.opacity = 0;
+            this.el.style.zIndex = '';
 
-        this.trigger('show', this);
-        return this;
-    },
-    hide: function () {
-        this.el.style.opacity = 0;
-        this.el.style.zIndex = '';
+            // waiting for the end opacity css transition
+            setTimeout(function () {
+                this.el.style.opacity = '';
+                this.trigger('hide', this);
+                try { document.body.removeChild(this.el); } catch(e){}
+            }.bind(this), 160);
 
-        // waiting for the end opacity css transition
-        setTimeout(function () {
-            this.el.style.opacity = '';
-            this.trigger('hide', this);
-            try { document.body.removeChild(this.el); } catch(e){}
-        }.bind(this), 160);
-
-        return this;
-    }
-}));
+            return this;
+        }
+    });
 
 
-// {model} Window settings
-var _window = new (Backbone.Model.extend({
-    defaults: {
-        width: 0,
-        height: 0
-    },
-    initialize: function() {
-        this._origin = window;
-    }
-}));
+    // {model} Window settings
+    var WindowModel = Backbone.Model.extend({
+        name: 'WindowModel',
+        defaults: {
+            width: 0,
+            height: 0
+        },
+        initialize: function() {
+            this._origin = window;
+        }
+    });
 
 
-// {model} Container
-var _container = new (Backbone.Model.extend({
-    defaults: {
-        width: 0,
-        height: 0,
-        format: ''
-    },
-    initialize: function() {
-        this.listenTo(_window, 'change:width', _.throttle(this.setFormat));
-        this.setFormat();
-    },
-    setFormat: function() {
-        var format,
-            win_width = _window.get('width');
+    // {model} Container
+    var ContainerModel = Backbone.Model.extend({
+        name: 'ContainerModel',
+        defaults: {
+            width: 0,
+            height: 0,
+            format: ''
+        },
+        setFormat: function() {
+            var format,
+                win_width = ui.window.get('width');
 
-        if(win_width >= 1360)
-            format = 'xdesktop';
-        else if(win_width >= 1180 && win_width <= 1359)
-            format = 'desktop';
-        else if(win_width >= 1024 && win_width <= 1179)
-            format = 'tablet';
-        else if(win_width >= 768 && win_width <= 1023)
-            format = 'tablet-portrait';
-        else if(win_width >= 480 && win_width <= 767)
-            format = 'handset';
-        else if(win_width <= 479)
-            format = 'handset-portrait';
+            if(win_width >= 1360)
+                format = 'xdesktop';
+            else if(win_width >= 1180 && win_width <= 1359)
+                format = 'desktop';
+            else if(win_width >= 1024 && win_width <= 1179)
+                format = 'tablet';
+            else if(win_width >= 768 && win_width <= 1023)
+                format = 'tablet-portrait';
+            else if(win_width >= 480 && win_width <= 767)
+                format = 'handset';
+            else if(win_width <= 479)
+                format = 'handset-portrait';
 
-        return this.set({
-            'format': format,
-            'height': Math.max($(document.body).height(), _window.get('height')),
-            'width': $('.o-container').eq(0).width()
-        });
-    }
-}));
+            return this.set({
+                'format': format,
+                'height': Math.max($(document.body).height(), ui.window.get('height')),
+                'width': $('.o-container').eq(0).width()
+            });
+        }
+    });
 
-console.log(root);
-//
-window.ui = {
-    backdrop: _backdrop,
-    container: _container,
-    window: _window,
+    //{class} UiCore
 
-    _components: {},
-    component: function(addon) {
-        var _component;
+    class UiCore {
+        constructor() {
+            this.backdrop = new BackdropView;
+            this.window = new WindowModel;
+            this.container = new ContainerModel;
 
-        typeof addon == 'function' && (_component = new addon(this));
-        typeof addon == 'object' && (_component = addon);
+            this.container.listenTo(this.window, 'change:width', _.throttle(this.container.setFormat));
+        }
 
-        if(_.isObject(_component)) {
-            this._components[_.camelize(_component.NAME)] = _component;
+        component(addon) {
+            let _component;
+
+            _.isFunction(addon) && (_component = new addon(this));
+            console.assert(_.isObject(_component), "Error initialization of the component.", _component);
+
+            _components[_.camelize(_component.NAME)] = _component;
             _.extend(_component, Backbone.Events);
             $(document).ready(_component.init.bind(_component));
-        } else
-            console.error("Error initialization of the component.", _component);
+
+            return this;
+        }
     }
-};
+
+    window.ui = new UiCore;
+    window.addEventListener('resize', _ons.window.resize, false);
+    $(document).ready(_ons.window.resize);
+}());
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const webpack = require('webpack'),
       path = require('path');
 
 module.exports = {
-    entry: __dirname + '/js/ui/core.js',
+    entry: './js/ui/core.js',
     output: {
         path: path.join(__dirname, "dist"),
-        filename: 'ui-core.js',
+        filename: 'ui-core.js?bust=' + (new Date),
         publicPath: './dist/'
     },
 	module: {
