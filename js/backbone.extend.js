3f6724f37affaa99b03a9ec508802a0c3fac32ad
BPanchenko <oberon@list.ru>
2016-09-18 13:03:28 +0300

    extends backbone.events: listenToAndRun and onAndRun methods

--- js/backbone.extend.js
+++ js/backbone.extend.js
@@ -1,16 +1,24 @@
 (function(Backbone){
 
-
     // @Events
 
-    _.extend(Backbone.Events, {
-        listenToAndRun: function(obj, name, callback) {
+    var eventMethods = {
+        listenToAndRun: function (obj, name, callback) {
             this.listenTo.apply(this, arguments);
             if (!callback && typeof name === 'object') callback = this;
-            callback.apply(this);
+            callback.call(this, obj);
+            return this;
+        },
+        onAndRun: function (name, callback) {
+            this.on.apply(this, arguments);
+            callback.call(this, this);
             return this;
         }
-    });
+    };
+    _.extend(Backbone.Events, eventMethods);
+    _.extend(Backbone.Collection.prototype, eventMethods);
+    _.extend(Backbone.Model.prototype, eventMethods);
+    _.extend(Backbone.View.prototype, eventMethods);
 
 
     // @View
