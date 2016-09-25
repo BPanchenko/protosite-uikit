8bddd09b036e123753b15a91433f7140bd0dda18
BPanchenko <oberon@list.ru>
2016-09-25 20:21:25 +0300

    add files for a 'dropbox' component

---
+++ js/ui/components/dropbox.js
@@ -0,0 +1,26 @@
+;(function(ui){
+
+    var _ons = {
+        drop: function(e){
+
+            return;
+        }
+    };
+
+    var DropdownComponent = function (elem, options) {
+        options || (options = {});
+
+        this.el = options.el;
+        _addEventListeners.call(this);
+    }
+
+    /** Events */
+
+    function _addEventListeners(){
+
+        return this;
+    }
+
+    /** Files in dropbox */
+
+}(ui));
