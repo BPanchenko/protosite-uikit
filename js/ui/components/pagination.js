7157f0a28bb5ab8325c061ec0cbe8ce0c299c3b0
BPanchenko <oberon@list.ru>
2016-12-12 23:24:15 +0300

    refact to es2015

--- js/ui/components/pagination.js
+++ js/ui/components/pagination.js
@@ -1,61 +1,69 @@
 (function(UI){
 
-    var tpl = '<div class="c-pagination__pages">'
-                + '<a class="c-pagination__prev" href="#prev" data-page="<%=(page - 1)%>" aria-disabled="<%= page == 1 %>"></a>'
-                + '<% if(page > edges + 1) { %>'
-                + '<a class="c-pagination__page" href="#p=1" data-page="1">1</a>'
-                + '<span class="c-pagination__ellipsis">&hellip;</span>'
-                + '<% } %>'
-                + '<% for(var i = start; i <= end; i++) if(i) { %>'
-                + '<a class="c-pagination__page" href="#p=<%=i%>" data-page="<%=i%>" aria-selected="<%= page == i %>"><%=i%></a>'
-                + '<% } %>'
-                + '<% if(page < total - edges) { %>'
-                + '<span class="c-pagination__ellipsis">&hellip;</span>'
-                + '<a class="c-pagination__page" href="#p=<%= total %>" data-page="<%=total%>"><%= total %></a>'
-                + '<% } %>'
-                + '<a class="c-pagination__next" href="#next" data-page="<%=(page + 1)%>" aria-disabled="<%= page == total %>"></a>'
-            + '</div>'
-            + '<div class="c-pagination__perpage">'
-                + '<a href="#select-per-page" class="c-pagination__perpage__current"><%=itemsOnPage%></a>'
-                + '<div class="c-pagination__perpage__values">'
-                + '<% itemsOnPageValues.forEach(function(iop){ %>'
-                + '<a href="#p=1&itemsOnPage=<%=iop%>"'
-                    + ' data-items_on_page="<%=iop%>"'
-                    + ' class="c-pagination__perpage__value"'
-                    + ' aria-selected="<%= itemsOnPage == iop %>"><%=iop%></a>'
-                + '<% });%>'
-                + '</div>'
-            + '</div>';
+    const tpl = `
+        <div class="c-pagination__pages">
+            <a class="c-pagination__prev" href="#prev" data-page="<%=(page - 1)%>" aria-disabled="<%= page == 1 %>"></a>
+            <% if(page > edges + 1) { %>
+            <a class="c-pagination__page" href="#p=1" data-page="1">1</a>
+            <span class="c-pagination__ellipsis">&hellip;</span>
+            <% } %>
+            <% for(var i = start; i <= end; i++) if(i) { %>
+            <a class="c-pagination__page" href="#p=<%=i%>" data-page="<%=i%>" aria-selected="<%= page == i %>"><%=i%></a>
+            <% } %>
+            <% if(page < total - edges) { %>
+            <span class="c-pagination__ellipsis">&hellip;</span>
+            <a class="c-pagination__page" href="#p=<%= total %>" data-page="<%=total%>"><%= total %></a>
+            <% } %>
+            <a class="c-pagination__next" href="#next" data-page="<%=(page + 1)%>" aria-disabled="<%= page == total %>"></a>
+        </div>
+        <div class="c-pagination__perpage">
+            <a href="#select-per-page" class="c-pagination__perpage__current"><%=itemsOnPage%></a>
+            <div class="c-pagination__perpage__values">
+            <% itemsOnPageValues.forEach(function(iop){ %>
+            <a href="#p=1&itemsOnPage=<%=iop%>"
+                 data-items_on_page="<%=iop%>"
+                 class="c-pagination__perpage__value"
+                 aria-selected="<%= itemsOnPage == iop %>"><%=iop%></a>
+            <% });%>
+            </div>
+        </div>
+    `;
 
-    var observer = new MutationObserver(function(mutations) {
-        var target = mutations[0].target;
-        var attrs = _.stringToObject(target.getAttribute('ui-pagination'));
+    const observer = new MutationObserver(mutations => {
+        let target = mutations[0].target;
+        let attrs = _.stringToObject(target.getAttribute('ui-pagination'));
         target._uiPagination.model.set(attrs);
     });
 
-    var paginationOptions = ['page','items','itemsOnPage','itemsOnPageValues'];
+    const paginationOptions = ['page', 'items', 'itemsOnPage', 'itemsOnPageValues'];
 
-    var PaginationModel = Backbone.Model.extend({
-        name: 'PaginationModel',
-        defaults: {
-            page: 1,
-            displayedPages: 7,
-            edges: 3,
-            items: 0,
-            itemsOnPage: 20,
-            itemsOnPageValues: [100, 50, 20]
-        }
-    });
-
-    var _hns = {
+    const _hns = {
         model: {
-            change: function(model){
-                this.el.setAttribute('ui-pagination', JSON.stringify(this.model.pick(paginationOptions)));
+            change: function(model) {
+                this.el.setAttribute('ui-pagination', JSON.stringify(model.pick(paginationOptions)));
                 this.render();
                 return;
             }
         }
     };
+
+    /** State model */
+
+    class PaginationModel extends Backbone.Model {
+        constructor(data) {
+            // defaults
+            data = Object.assign({
+                page: 1,
+                displayedPages: 7,
+                edges: 3,
+                items: 0,
+                itemsOnPage: 20,
+                itemsOnPageValues: [100, 50, 20]
+            }, data);
+            // constructor
+            super(data);
+        }
+    }
 
     /** Class component */
 
@@ -95,18 +103,20 @@
             },
 
             initialize: function(){
+                if(_.isString(this.options)) this.options = _.stringToObject(this.options);
+
                 this.model = new PaginationModel(_.pick(this.options, paginationOptions));
-                this.listenTo(this.model, 'change', _hns.model.change);
+                this.listenToAndRun(this.model, 'change', _hns.model.change);
 
                 observer.observe(this.el, {
                     attributes: true,
-                    attributeFilter: ['ui-pagination']
+                    attributeFilter: [this.constructor.attr]
                 });
             },
 
             template: function () {
                 var state = this.model.toJSON(),
-                    data = _.extend({
+                    data = Object.assign({
                         start: state.page - state.edges,
                         end: state.edges + state.page,
                         total: Math.ceil(state.items/state.itemsOnPage)
@@ -127,7 +137,7 @@
                 return _.template(tpl)(data);
             },
             render: function () {
-                this.el.innerHTML = _.result(this, 'template');
+                this.el.innerHTML = this.template();
                 this.el.setAttribute('aria-disable', !this.model.get('items'));
                 return this;
             }
@@ -136,12 +146,19 @@
 
     /** Init component on elements */
 
-    UI.dom.on('ready change', function(doc, options){
-        $(UI.Pagination.selector).each(function(){
-            if(!this[UI.Pagination.link]) {
-                console.log((new UI.Pagination(this, _.stringToObject(this.getAttribute(UI.Pagination.attr)))).render());
-            }
-        });
+    function _init(elems){
+        Array.from(elems)
+            .filter(elem => elem.hasAttribute(UI.Pagination.attr) && !elem[UI.Pagination.reference])
+            .forEach(elem => new UI.Pagination(elem, elem.getAttribute(UI.Pagination.attr)));
+        return;
+    }
+
+    UI.dom.on('ready', function(doc){
+        _init(doc.querySelectorAll(UI.Pagination.selector));
+    });
+
+    UI.dom.on('change', function(doc, options){
+        _init(options.added);
     });
 
 }(UI));
