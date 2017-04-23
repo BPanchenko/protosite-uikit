cbd8cc920a5916c882442a858848ba2cedeb4801
BPanchenko <oberon@list.ru>
2017-04-24 00:13:21 +0300

    bugfix collection prototype

--- js/modules/backbone-addon.js
+++ js/modules/backbone-addon.js
@@ -1,28 +1,9 @@
 (function(Backbone){
 
-  // @Events
-
-  var eventMethods = {
-    listenToAndRun: function (obj, name, callback) {
-      this.listenTo.apply(this, arguments);
-      if (!callback && typeof name === 'object') callback = this;
-      callback.call(this, obj);
-      return this;
-    },
-    onAndRun: function (name, callback) {
-      this.on.apply(this, arguments);
-      callback.call(this, this);
-      return this;
-    }
-  };
-  _.extend(Backbone.Events, eventMethods);
-  _.extend(Backbone.Collection.prototype, eventMethods);
-  _.extend(Backbone.Model.prototype, eventMethods);
-  _.extend(Backbone.View.prototype, eventMethods);
-
-
   // @Collection
-  
+
+  var _collectionPrototype = Backbone.Collection.prototype;
+
   Backbone.Collection = function(models, options) {
     options || (options = {});
     if (options.model) this.model = options.model;
@@ -33,7 +14,7 @@
     if (models) this.reset(models, _.extend({silent: true}, options));
   };
 
-  _.extend(Backbone.Collection.prototype, {
+  _.extend(Backbone.Collection.prototype, _collectionPrototype, {
     parse: function(response) {
       var list = [];
       if(_.isArray(response)) {
@@ -55,47 +36,70 @@
   // @View
 
   _.extend(Backbone.View.prototype, {
-      elems: {},
-      tpl: '',
-      template: function() {
-        let data = this.model ? this.model.toJSON() : {};
-        return _.template(this.tpl)(data);
-      },
-      render: function() {
-        if(this._isRendered && _.isFunction(this.reRender)) {
-          return this.reRender();
-        }
 
-        this.el.innerHTML = _.result(this, 'template');
-        this._initElems();
-        this.onRender && this.onRender.call(this);
-        this._isRendered = true;
+    tpl: '',
+    template: function() {
+      var data = this.model ? this.model.toJSON() : {};
+      return _.template(this.tpl)(data);
+    },
+    render: function() {
+      if(this._isRendered && _.isFunction(this.reRender)) {
+        return this.reRender();
+      }
 
-        return this;
-      },
-      _initElems: function() {
-        if(_.isEmpty(this.elems)) return this;
+      this.el.innerHTML = _.result(this, 'template');
+      this._initElems();
+      this.onRender && this.onRender.call(this);
+      this._isRendered = true;
 
-        if(!(this.el instanceof HTMLElement)) {
-          console.trace("HTML-container is undefined");
-          return this;
-        }
+      return this;
+    },
 
-        if(!this.__proto__.selectors) {
-          // cache selectors of elements
-          this.__proto__.selectors = _.clone(this.elems);
-        }
-        _.each(this.selectors, function(selector, key){
-          if(_.isString(selector)) {
-            this.elems[key] = this.el.getElementsByClassName(selector);
-            console.assert(this.elems[key].length, this._name + ", element '" + selector + "' was not found");
-            if(this.elems[key].length == 1) this.elems[key] = this.elems[key][0];
-          }
-          return;
-        }, this);
+    _initElems: function() {
+      if(_.isEmpty(this.elems)) return this;
 
+      if(!(this.el instanceof HTMLElement)) {
+        console.trace("HTML-container is undefined");
         return this;
       }
+
+      if(!this.__proto__.selectors) {
+        // cache selectors of elements
+        this.__proto__.selectors = _.clone(this.elems);
+      }
+
+      _.each(this.selectors, function(selector, key){
+        if(_.isString(selector)) {
+          this.elems[key] = this.el.getElementsByClassName(selector);
+          console.assert(this.elems[key].length, this._name + ", element '" + selector + "' was not found");
+          if(this.elems[key].length == 1) this.elems[key] = this.elems[key][0];
+        }
+        return;
+      }, this);
+
+      return this;
+    }
   });
+
+  // @Events
+
+  var eventMethods = {
+    listenToAndRun: function (obj, name, callback) {
+      this.listenTo.apply(this, arguments);
+      if (!callback && typeof name === 'object') callback = this;
+      callback.call(this, obj);
+      return this;
+    },
+    onAndRun: function (name, callback) {
+      this.on.apply(this, arguments);
+      callback.call(this, this);
+      return this;
+    }
+  };
+
+  _.extend(Backbone.Events, eventMethods);
+  _.extend(Backbone.Collection.prototype, eventMethods);
+  _.extend(Backbone.Model.prototype, eventMethods);
+  _.extend(Backbone.View.prototype, eventMethods);
 
 }(Backbone));
