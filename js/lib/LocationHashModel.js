6fb121038d0538653a954ae4c6470812031b046a
BPanchenko <oberon@list.ru>
2017-06-03 22:56:52 +0300

    model state of the window location hash

---
+++ js/lib/LocationHashModel.js
@@ -0,0 +1,39 @@
+(function(_, Backbone) {
+  
+  window.location.hashModel = new (Backbone.Model.extend({
+    initialize: function () {
+      this.on('change', function (model, options) {
+        if (options.fromWindowHash) return;
+        var query = _.objectToQuery(this.toJSON());
+        if (getHash() != query) window.location.hash = query;
+        return;
+      });
+    },
+    getQuery: function(options = {}) {
+      var attrs;
+      if(options.omit) attrs = this.omit.apply(this, _.isString(options.omit) ? options.omit.split(',') : options.omit);
+      else attrs = this.toJSON();
+      return _.objectToQuery(attrs);
+    }
+  }));
+
+  window.addEventListener("hashchange", onWindowHashChange, true);
+  onWindowHashChange();
+
+  function getHash() {
+    return window.location.hash.substring(1);
+  }
+
+  function onWindowHashChange() {
+    var data = _.queryToObject(getHash());
+    var model = window.location.hashModel;
+    if (_.isEmpty(data)) {
+      model.clear();
+    } else {
+      _.difference(model.keys(), _.keys(data)).forEach(key => model.unset(key));
+      model.set(data, { fromWindowHash: true });
+    }
+    return;
+  }
+
+}(_, Backbone));
