93e23d4813a90ec34df72bdf5bd133ec1a677f13
BPanchenko <oberon@list.ru>
2017-06-03 21:44:16 +0300

    add methods `checkEmail`, `checkUrl`, `queryToObject` and `parseUrl`

--- js/modules/underscore-mixin.js
+++ js/modules/underscore-mixin.js
@@ -2,11 +2,39 @@
     s && _.mixin(s.exports());
 
     _.mixin({
+        'checkEmail': function (str) {
+            return /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i.test(str);
+        },
+        'checkUrl': function (str) {
+            return /^(?:\S+)\.(?:\S+)$/i.test(str);
+        },
+        'queryToObject': function (str) {
+            var obj = {};
+            typeof str == 'string' && str && str.split('&').forEach(function (p) {
+                var t = p.split('='), v = t[1] ? decodeURIComponent(t[1]) : null;
+                obj[decodeURIComponent(t[0])] = /^\d+$/.test(v) ? +v : v;
+            });
+            return obj;
+        },
         'objectToQuery': function(obj) {
             return Object.keys(obj).map(function(key) {
                 return encodeURIComponent(key) + '=' +
                     encodeURIComponent(obj[key]);
             }).join('&');
+        },
+        'parseUrl': function (str) {
+            var _temp = /^(?:([a-z0-9_\-\.]+):\/\/)*(?:([a-z0-9_\-\.]+)(?:\:)*([a-z0-9_\-\.]+)*\@)*([a-z0-9][a-z0-9_\-\.]+)(?:\:([\d]+))*(?:\/([^?#]*))*(?:\?([^?#]*))*(?:\#([^?#]*))*/gi.exec(str);
+
+            return {
+                scheme: _temp[1] || false,
+                user: _temp[2] || false,
+                pass: _temp[3] || false,
+                host: _temp[4] || false,
+                port: _temp[5] || false,
+                path: _temp[6] || false,
+                query: _temp[7] || false,
+                fragment: _temp[8] || false
+            };
         },
         'stringToObject': function(string) {
             string = string.replace(/'/g, '"');
