9c1d07602982e414dd644c2c5d931c37c08fe535
Boris Panchenko <hello@bpanchenko.ru>
2018-03-01 22:59:52 +0300

    upgrade of initialization of custom element button

--- src/js/ui-core.js
+++ src/js/ui-core.js
@@ -1,9 +1,11 @@
 import UI from './ui/index';
+import ButtonElement from './custom-elements/button';
 
 import LocationHashModel from './ui/lib/LocationHashModel';
 
 import './ui/components/popover';
 import './ui/components/field';
 
+console.log(ButtonElement);
 window.LocationHashModel = LocationHashModel;
 window.UI = UI;
