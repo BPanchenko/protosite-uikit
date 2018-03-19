ceb94a22a094e8bff74a34725ed2bb30d8a3725d
Boris Panchenko <hello@bpanchenko.ru>
2018-03-19 23:19:09 +0300

    replace webpack on rollup

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,8 @@ import UI from './ui/index';
 
 import LocationHashModel from './ui/lib/LocationHashModel';
 
+import './components';
 import './custom-elements/button';
-import './ui/components/popover';
-import './ui/components/field';
 
-console.log(ButtonElement);
 window.LocationHashModel = LocationHashModel;
 window.UI = UI;
