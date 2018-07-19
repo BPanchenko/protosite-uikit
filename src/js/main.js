72cfcb28aea50682f3d8398ddb962db82aa4e5cb
Boris Panchenko <hello@bpanchenko.ru>
2018-07-19 22:55:49 +0300

    define a custom elements with using imported modules

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,7 @@
-process.env.NODE_ENV !== 'prod' && console.time('t');
+console.time('t');
 
-import '../less/core.less';
-import './custom-elements/button';
+import './custom-elements/button.js';
+import './custom-elements/field.js';
+import './custom-elements/tabs.js';
 
-import Vue from 'vue/dist/vue.esm.js';
-import components from './components';
-import directives from './directives';
-
-components.install(Vue);
-directives.install(Vue);
-
-global.Vue = Vue;
-
-process.env.NODE_ENV !== 'prod' && console.timeEnd('t');
+console.timeEnd('t');
