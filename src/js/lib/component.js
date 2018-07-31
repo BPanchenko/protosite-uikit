fc292e7273e5bf510d6eb17615e22ed8f334419d
Boris Panchenko <hello@bpanchenko.ru>
2018-07-31 23:00:34 +0300

    create a component element

--- src/js/lib/component.js
+++ src/js/lib/component.js
@@ -3,21 +3,27 @@ import Store from '../store/store.js';
 
 export default class Component {
     constructor(props = {}) {
-        let self = this;
-
-        // We're setting a render function as the one set by whatever inherits this base
-        // class or setting it to an empty by default. This is so nothing breaks if someone
-        // forgets to set it.
-        this.render = this.render || function() {};
 
         // If there's a store passed in, subscribe to the state change
         if(props.store instanceof Store) {
-            props.store.events.subscribe('stateChange', () => self.render());
+            this.store = props.store;
+            this.store.events.subscribe('stateChange', () => this.render());
         }
 
         // Store the HTML element to attach the render to if set
         if(props.hasOwnProperty('element')) {
             this.element = props.element;
+        } else {
+            this.element = document.createElement(this.tagName);
+            if(Array.isArray(this.className)) this.element.classList.add(...this.className);
+            else if(this.className) this.element.classList.add('' + this.className);
         }
     }
+
+    render() {
+        return this;
+    }
+
+    get tagName() { return 'div'; }
+    get className() { return null; }
 }
