0bd5a65b7c8fb4d3e59964c11f79a57c19dc2153
Boris Panchenko <hello@bpanchenko.ru>
2018-08-02 22:27:12 +0300

    fix version

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import '../less/core.less';
-import UI from './ui/index';
-
-import LocationHashModel from './ui/lib/LocationHashModel';
-
-import './components';
-import './custom-elements/button';
-
-window.LocationHashModel = LocationHashModel;
-window.UI = UI;
-=======
 console.time('t');
 
 import './custom-elements/button.js';
@@ -18,4 +5,3 @@ import './custom-elements/field.js';
 import './custom-elements/tabs.js';
 
 console.timeEnd('t');
->>>>>>> remotes/origin/dev-vuejs-rollup
