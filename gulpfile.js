fe864dd74bc8fcff58c41485ae53f89e9f1d028f
Boris Panchenko <hello@bpanchenko.ru>
2019-01-20 21:26:34 +0300

    bugfix gulp build css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const postcss = require('gulp-postcss');
 const postcssPresetEnv = require('postcss-preset-env');
+const postcssImport = require('postcss-import');
 const gulp = require('gulp');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
@@ -9,7 +10,8 @@ gulp.task('build-css', function () {
     let plugins = [
         autoprefixer({ browsers: ['last 1 version'] }),
         cssnano(),
-        postcssPresetEnv({ stage: 0 })
+        postcssPresetEnv({ stage: 0 }),
+        postcssImport()
     ];
     return gulp.src('./src/css/protosite-uikit.css')
         .pipe(postcss(plugins, { parser: sugarss }))
