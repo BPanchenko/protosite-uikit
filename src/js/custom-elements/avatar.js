0de06a556d449295c4af43c2e63531958fd11577
oberon <oberon@list.ru>
2019-02-27 22:51:15 +0300

    avatar custom element

--- src/js/custom-elements/avatar.js
+++ src/js/custom-elements/avatar.js
@@ -15,6 +15,8 @@
 
     const CLS = Object.create(null, {
         'avatar': { value: 'c-avatar' },
+        'link': { value: 'c-avatar__link' },
+        'image': { value: 'c-avatar__image' },
         'xs': { value: 'c-avatar--xs' },
         'sm': { value: 'c-avatar--sm' },
         'md': { value: 'c-avatar--md' },
@@ -24,7 +26,7 @@
 
     class AvatarElement extends HTMLElement {
         connectedCallback() {
-            this.render();
+            this.render().cleanup();
         }
 
         render() {
@@ -32,19 +34,53 @@
             this._figure.classList.add(CLS.avatar);
             this.appendChild(this._figure);
 
-            let { src, href, size, shadow } = this.dataset;
+            let { src, href, size, shadow, target } = this.dataset;
             
-            if (src) {}
-            if (href) {}
-            
-            if (~[
-                'xsmall','small','medium','large','xlarge',
-                'xs','sm','md','lg','xl'
-            ].indexOf(size)) {
-                
+            if (href) {
+                this._link = document.createElement('a');
+                this._link.classList.add(CLS.link);
+                this._link.href = href;
+                this._link.target = target || '_self';
+                this._figure.appendChild(this._link);
             }
 
-            if (shadow) {}
+            if (src) {
+                if (shadow == 'inset') {
+                    this._figure.style.backgroundImage = `url(${src})`;
+                } else {
+                    this._img = document.createElement('img');
+                    this._img.classList.add(CLS.image);
+                    this._img.src = src;
+                    if (this._link) {
+                        this._link.appendChild(this._img);
+                    } else {
+                        this._figure.appendChild(this._img);
+                    }
+                }
+            }
+            
+            if (~['xs','sm','md','lg','xl'].indexOf(size)) {
+                this._figure.classList.add(CLS[size]);
+            } else if (size) {
+                console.warn("Size can take values 'xs', 'sm', 'md', 'lg' or 'xl'");
+            }
+
+            if (~['2dp','3dp','4dp','6dp','8dp','16dp','24dp'].indexOf(shadow)) {
+                this._figure.classList.add(`s-shadow-${shadow}`);
+            } else if (shadow && shadow != 'inset') {
+                console.warn("Shadow can take values '2dp', '3dp', '4dp', '6dp', '8dp', '16dp' or '24dp'");
+            }
+
+            return this;
+        }
+
+        cleanup() {
+            this.removeAttribute('data-src');
+            this.removeAttribute('data-href');
+            this.removeAttribute('data-size');
+            this.removeAttribute('data-shadow');
+            this.removeAttribute('data-target');
+            return this;
         }
     }
 
