d401fe008b403de5db6de34c838f1a62fb0dea3d
oberon <oberon@list.ru>
2019-04-02 07:30:39 +0300

    show and hide popover

--- src/js/custom-elements/popover.js
+++ src/js/custom-elements/popover.js
@@ -15,17 +15,24 @@
 
     const CLS = Object.create(null, {
         main: { value: 'c-popover' },
-        body: { value: 'c-popover__body' }
+        body: { value: 'c-popover__body' },
+        show: { value: 'is-visible' },
+        hide: { value: 'is-hidden' }
     });
     
     /* Element Class
      ========================================================================== */
 
     class PopoverElement extends HTMLElement {
+        
         connectedCallback() {
             this._control = document.body.querySelector(`*[aria-controls=${this.id}]`);
+            console.log(this._control);
             this.render();
             addEventListeners.call(this);
+        }
+        disconnectedCallback() {
+            removeEventListeners.call(this);
         }
 
         render() {
@@ -33,11 +40,32 @@
             this.innerHTML = '';
             this.classList.add(CLS.main);
             this._body = createBody.call(this);
+            this.hide();
             return this;
         }
 
-        disconnectedCallback() {
-            removeEventListeners.call(this);
+        onControlEnter() {
+
+            return;
+        }
+        onControlLeave() {
+
+            return;
+        }
+
+        placement() {
+            return this;
+        }
+
+        show() {
+            this.classList.add(CLS.show);
+            this.classList.remove(CLS.hide);
+            return this;
+        }
+        hide() {
+            this.classList.add(CLS.hide);
+            this.classList.remove(CLS.show);
+            return this;
         }
     }
     
@@ -45,7 +73,11 @@
      ========================================================================== */
 
     function addEventListeners() {
-        
+        this.__onEnterControl = () => this.show().placement();
+        this.__onLeaveControl = () => this.hide();
+        this._control.addEventListener('mouseenter', this.__onEnterControl);
+        this._control.addEventListener('mouseleave', this.__onLeaveControl);
+        return this;
     }
 
     function createBody() {
@@ -54,6 +86,12 @@
         elem.innerHTML = this._innerHTML;
         this.appendChild(elem);
         return elem;
+    }
+
+    function removeEventListeners() {
+        this._control.removeEventListener('mouseenter', this.__onEnterControl);
+        this._control.removeEventListener('mouseleave', this.__onLeaveControl);
+        return;
     }
     
     /* Define the new element
