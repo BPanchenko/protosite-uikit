b6a973adbaf80bfd5b4befda4527443b2048dbee
Boris Panchenko <oberon@list.ru>
2019-12-15 11:35:21 +0300

    rollback configs

---
+++ .config/postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    parser: false,
+    plugins: {
+        'postcss-color-function': {},
+        'postcss-custom-properties': {},
+        'postcss-import': {},
+        'postcss-preset-env': { stage: 0 },
+        'cssnano': {}
+    }
+};
---
+++ .config/webpack.config.js
@@ -0,0 +1,52 @@
+// Webpack v4
+
+const path = require('path');
+const webpack = require('webpack');
+
+let config = {
+    entry: ['./src/js/uikit.js'],
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'protosite-uikit.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    { loader: 'postcss-loader', options: { config: { path: './.config' } } }
+                ]
+            }
+        ]
+    },
+    plugins: []
+};
+
+module.exports = (env, argv) => {
+    let inDevelopment = argv.mode === 'development';
+    let inProduction = argv.mode === 'production';
+
+    if (inDevelopment) {
+        config.devServer = {
+            contentBase: path.resolve(__dirname, '../'),
+            hot: true,
+            inline: true,
+            open: 'Chrome',
+            publicPath: '/dist/',
+            watchContentBase: true,
+            watchOptions: {
+                poll: true
+            }
+        };
+        config.devtool = 'eval-source-map';
+        config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    }
+
+    if (inProduction) {
+        config.devtool = 'source-map';
+    }
+
+    return config;
+};
