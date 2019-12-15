d94b55b438f40e43cca1789e4b994570879dbbec
Boris Panchenko <oberon@list.ru>
2019-12-15 10:00:02 +0300

    the build styles in a Assets folder

--- .config/gulpfile.js
+++ .config/gulpfile.js
@@ -4,15 +4,27 @@ const postcssImport = require('postcss-import');
 const gulp = require('gulp');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const rename = require("gulp-rename");
 
-gulp.task('build-css', function () {
-    let plugins = [
-        autoprefixer({ browsers: ['> 5%'] }),
-        postcssImport(),
-        postcssPresetEnv({ stage: 0 }),
-        cssnano()
-    ];
-    return gulp.src('../src/css/protosite-uikit.css')
-        .pipe(postcss(plugins, { parser: false }))
-        .pipe(gulp.dest('../dist'));
+let postcss_plugins = [
+    autoprefixer({ browsers: ['> 5%'] }),
+    postcssImport(),
+    postcssPresetEnv({ stage: 0 }),
+    cssnano()
+];
+
+gulp.task('css-theme-article', function () {
+    return gulp.src('../src/css/themes/article.css')
+        .pipe(postcss(postcss_plugins, { parser: false }))
+        .pipe(rename('uikit-theme-article.css'))
+        .pipe(gulp.dest('../assets'));
 });
+
+gulp.task('css-uikit', function () {
+    return gulp.src('../src/css/protosite-uikit.css')
+        .pipe(postcss(postcss_plugins, { parser: false }))
+        .pipe(rename('uikit.css'))
+        .pipe(gulp.dest('../assets'));
+});
+
+gulp.task('build-css', gulp.series(gulp.parallel('css-theme-article', 'css-uikit')));
