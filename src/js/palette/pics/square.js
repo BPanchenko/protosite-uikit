481d2fcc5fe673e09be924c407926dff686927ac
Boris Panchenko <oberon@list.ru>
2020-12-30 13:53:08 +0300

    Project Settings

---
+++ src/js/palette/pics/square.js
@@ -0,0 +1,4 @@
+
+export default function (...args) {
+    console.log('renderSquare()', args)
+}
---
+++ src/js/palette/settings.js
@@ -0,0 +1,27 @@
+
+/* Constants
+    ========================================================================== */
+
+const COLORS = new Map([
+    ['red', 'Red'],
+    ['pink', 'Pink'],
+    ['purple', 'Purple'],
+    ['violet', 'Violet (Deep Purple)'],
+    ['indigo', 'Indigo'],
+    ['blue', 'Blue'],
+    ['light-blue', 'Light Blue'],
+    ['cyan', 'Cyan'],
+    ['teal', 'Teal'],
+    ['green', 'Green'],
+    ['light-green', 'Light Green'],
+    ['lime', 'Lime'],
+    ['yellow', 'Yellow'],
+    ['amber', 'Amber'],
+    ['orange', 'Orange'],
+    ['deep-orange', 'Deep Orange'],
+    ['brown', 'Brown'],
+    ['grey', 'Grey'],
+    ['blue-grey', 'Blue Grey'],
+    ['black', 'Black'],
+    ['white', 'White']
+])
