5ac04345ab0e244ee226a798d3de562584c173ee
Boris Panchenko <oberon@list.ru>
2021-01-01 22:21:14 +0300

    add `hex` property to map of colors

--- src/js/palette/settings.js
+++ src/js/palette/settings.js
@@ -3,43 +3,133 @@
   ========================================================================== */
 
 const COLORS = new Map([
-  ['red', { name: 'Red' }],
-  ['pink', { name: 'Pink' }],
-  ['purple', { name: 'Purple' }],
-  ['violet', { name: 'Violet (Deep Purple)' }],
-  ['indigo', { name: 'Indigo' }],
-  ['blue', {  name: 'Blue' }],
-  ['light-blue', { name: 'Light Blue' }],
-  ['cyan', { name: 'Cyan' }],
-  ['teal', { name: 'Teal' }],
-  ['green', { name: 'Green' }],
-  ['light-green', { name: 'Light Green' }],
-  ['lime', { name: 'Lime' }],
-  ['yellow', { name: 'Yellow' }],
-  ['amber', { name: 'Amber' }],
-  ['orange', { name: 'Orange' }],
-  ['deep-orange', { name: 'Deep Orange' }],
-  ['brown', { name: 'Brown' }],
-  ['grey', { name: 'Grey' }],
-  ['blue-grey', { name: 'Blue Grey' }],
-  ['black', { name: 'Black' }],
-  ['white', { name: 'White' }]
+  [
+    'red', {
+      name: 'Red',
+      hex: ''
+    }
+  ], [
+    'pink', {
+      name: 'Pink',
+      hex: ''
+    }
+  ], [
+    'purple', {
+      name: 'Purple',
+      hex: ''
+    }
+  ], [
+    'violet', {
+      name: 'Violet (Deep Purple)',
+      hex: ''
+    }
+  ], [
+    'indigo', {
+      name: 'Indigo',
+      hex: ''
+    }
+  ], [
+    'blue', {
+      name: 'Blue',
+      hex: ''
+    }
+  ], [
+    'light-blue', {
+      name: 'Light Blue',
+      hex: ''
+    }
+  ], [
+    'cyan', {
+      name: 'Cyan',
+      hex: ''
+    }
+  ], [
+    'teal', {
+      name: 'Teal',
+      hex: ''
+    }
+  ], [
+    'green', {
+      name: 'Green',
+      hex: ''
+    }
+  ], [
+    'light-green', {
+      name: 'Light Green',
+      hex: ''
+    }
+  ], [
+    'lime', {
+      name: 'Lime',
+      hex: ''
+    }
+  ], [
+    'yellow', {
+      name: 'Yellow',
+      hex: ''
+    }
+  ], [
+    'amber', {
+      name: 'Amber',
+      hex: ''
+    }
+  ], [
+    'orange', {
+      name: 'Orange',
+      hex: ''
+    }
+  ], [
+    'deep-orange', {
+      name: 'Deep Orange',
+      hex: ''
+    }
+  ], [
+    'brown', {
+      name: 'Brown',
+      hex: ''
+    }
+  ], [
+    'grey', {
+      name: 'Grey',
+      hex: ''
+    }
+  ], [
+    'blue-grey', {
+      name: 'Blue Grey',
+      hex: ''
+    }
+  ], [
+    'black', {
+      name: 'Black',
+      hex: '000000'
+    }
+  ], [
+    'white', {
+      name: 'White',
+      hex: 'FFFFFF'
+    }
+  ]
 ])
+
+/*
+  TODO:
+  document.documentElement.style.getPropertyValue('--clr-red') <= it's don't work
 
 COLORS.forEach((value, key, map) => {
   value.value = document.documentElement.style.getPropertyValue(`--clr-${key}`)
   console.log(`--clr-${key}`, document.documentElement.style.getPropertyValue(`--clr-${key}`))
 })
+*/ 
 
-// 
+const PALETTE = new Map(COLORS)
 
-const PALETTE = Object.create(COLORS, {
-  'text': {
-    name: 'Text Color'
-  },
-  'link': {
-    name: 'Link Color'
-  }
+PALETTE.set('text', {
+  name: 'Text Color',
+  hex: '333333'
+})
+PALETTE.set('link', {
+  name: 'Link Color',
+  hex: '1565C0'
 })
 
 export {
