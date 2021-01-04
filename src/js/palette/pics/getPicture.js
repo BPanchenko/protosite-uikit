9564fddf28084dc6adb31fad063c4839a768d949
Boris Panchenko <oberon@list.ru>
2021-01-04 20:48:55 +0300

    scene as argument of a render function

--- src/js/palette/pics/getPicture.js
+++ src/js/palette/pics/getPicture.js
@@ -12,13 +12,17 @@ export default function ({
   const camera = new THREE.OrthographicCamera(-1, 1, 1, -1)
   camera.position.set(0, 0, 10)
 
-  const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true
+  })
 
   renderer.setClearColor(new THREE.Color(0xEEEEEE))
   renderer.setSize(width, height)
   renderer.shadowMap.enabled = true
 
   scene.add(camera)
+  render(scene)
 
   return renderer.domElement
 }
