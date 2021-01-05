d87a05a37ec6db3f2cab160321d58d3a21b94885
Boris Panchenko <oberon@list.ru>
2021-01-05 09:01:25 +0300

    render background (square)

--- src/js/palette/pics/renderNineCircles.js
+++ src/js/palette/pics/renderNineCircles.js
@@ -3,11 +3,33 @@ import {
   parseColors
 } from '../library.js'
 
+const RADIUS = 0.2
+
+function background (color) {
+
+  const material = new THREE.MeshBasicMaterial({ color: `#${color.hex}` })
+
+  const shape = new THREE.ShapeGeometry(
+    new THREE.Shape([
+      new THREE.Vector2(-1, 1),
+      new THREE.Vector2(1, 1),
+      new THREE.Vector2(1, -1),
+      new THREE.Vector2(-1, -1),
+      new THREE.Vector2(-1, 1)
+    ])
+  )
+
+  const mesh = new THREE.Mesh(shape, material)
+  mesh.renderOrder = 1
+  console.log(mesh)
+  return mesh
+}
+
 export default function ({ colors }) {
   colors = parseColors(colors)
   console.assert(colors.length === 3, `Wrong amount of colors: ${JSON.stringify(colors)}`)
 
   return function (scene) {
-
+    scene.add(background(colors[0]))
   }
 }
