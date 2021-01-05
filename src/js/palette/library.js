7ab5ec5aef147e31c51b3104fb745459ac8b45ea
Boris Panchenko <oberon@list.ru>
2021-01-05 15:44:35 +0300

    move drawing functions to the library

--- src/js/palette/library.js
+++ src/js/palette/library.js
@@ -3,11 +3,45 @@ import * as _ from 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.12.0/
 
 import { PALETTE } from './settings.js'
 
-const parseColors = list => _.compact(list.map(item => PALETTE.has(item) ? PALETTE.get(item) : null))
+let parseColors = list => _.compact(list.map(item => PALETTE.has(item) ? PALETTE.get(item) : null))
+
+function square (color) {
+  let material = new THREE.MeshBasicMaterial({ color: `#${color.hex}` })
+
+  let shape = new THREE.ShapeGeometry(
+    new THREE.Shape([
+      new THREE.Vector2(-1, 1),
+      new THREE.Vector2(1, 1),
+      new THREE.Vector2(1, -1),
+      new THREE.Vector2(-1, -1),
+      new THREE.Vector2(-1, 1)
+    ])
+  )
+
+  let mesh = new THREE.Mesh(shape, material)
+  mesh.renderOrder = 1
+
+  return mesh
+}
+
+function circle (color, x, y, radius = 0.1) {
+  let segments = 64
+  let material = new THREE.MeshBasicMaterial({ color: `#${color.hex}` })
+  let geometry = new THREE.CircleGeometry(radius, segments)
+
+  let mesh = new THREE.Mesh(geometry, material)
+  mesh.matrix.setPosition(x, y, 0)
+  mesh.matrixAutoUpdate = false
+  mesh.renderOrder = 2
+
+  return mesh
+}
 
 export {
   _,
   PALETTE,
   THREE,
+  circle,
+  square,
   parseColors
 }
--- src/js/palette/pics/renderNineCircles.js
+++ src/js/palette/pics/renderNineCircles.js
@@ -1,41 +1,11 @@
 import {
   THREE,
+  circle,
+  square,
   parseColors
 } from '../library.js'
 
 const RADIUS = 0.13
-
-function square (color) {
-  let material = new THREE.MeshBasicMaterial({ color: `#${color.hex}` })
-
-  let shape = new THREE.ShapeGeometry(
-    new THREE.Shape([
-      new THREE.Vector2(-1, 1),
-      new THREE.Vector2(1, 1),
-      new THREE.Vector2(1, -1),
-      new THREE.Vector2(-1, -1),
-      new THREE.Vector2(-1, 1)
-    ])
-  )
-
-  let mesh = new THREE.Mesh(shape, material)
-  mesh.renderOrder = 1
-
-  return mesh
-}
-
-function circle (color, x, y, radius = RADIUS) {
-  let segments = 64
-  let material = new THREE.MeshBasicMaterial({ color: `#${color.hex}` })
-  let geometry = new THREE.CircleGeometry(radius, segments)
-
-  let mesh = new THREE.Mesh(geometry, material)
-  mesh.matrix.setPosition(x, y, 0)
-  mesh.matrixAutoUpdate = false
-  mesh.renderOrder = 2
-
-  return mesh
-}
 
 export default function ({ colors }) {
   colors = parseColors(colors)
@@ -43,14 +13,14 @@ export default function ({ colors }) {
 
   return function (scene) {
     scene.add(square(colors[0]))
-    scene.add(circle(colors[1], -0.618 + RADIUS, 0.618 - RADIUS))
-    scene.add(circle(colors[1], 0, 0.618 - RADIUS))
-    scene.add(circle(colors[2], 0.618 - RADIUS, 0.618 - RADIUS))
-    scene.add(circle(colors[1], -0.618 + RADIUS, 0))
-    scene.add(circle(colors[2], 0, 0))
-    scene.add(circle(colors[1], 0.618 - RADIUS, 0))
-    scene.add(circle(colors[2], -0.618 + RADIUS, -0.618 + RADIUS))
-    scene.add(circle(colors[1], 0, -0.618 + RADIUS))
-    scene.add(circle(colors[1], 0.618 - RADIUS, -0.618 + RADIUS))
+    scene.add(circle(colors[1], -0.618 + RADIUS, 0.618 - RADIUS, RADIUS))
+    scene.add(circle(colors[1], 0, 0.618 - RADIUS, RADIUS))
+    scene.add(circle(colors[2], 0.618 - RADIUS, 0.618 - RADIUS, RADIUS))
+    scene.add(circle(colors[1], -0.618 + RADIUS, 0, RADIUS))
+    scene.add(circle(colors[2], 0, 0, RADIUS))
+    scene.add(circle(colors[1], 0.618 - RADIUS, 0, RADIUS))
+    scene.add(circle(colors[2], -0.618 + RADIUS, -0.618 + RADIUS, RADIUS))
+    scene.add(circle(colors[1], 0, -0.618 + RADIUS, RADIUS))
+    scene.add(circle(colors[1], 0.618 - RADIUS, -0.618 + RADIUS, RADIUS))
   }
 }
