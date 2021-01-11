9aadb72948f63e79480e31795406eefee154ca07
Boris Panchenko <oberon@list.ru>
2021-01-11 08:01:29 +0300

    fix

--- src/js/palette/pics/renderSixCircles.js
+++ src/js/palette/pics/renderSixCircles.js
@@ -18,8 +18,8 @@ console.assert(colors.length === 3, `Wrong amount of colors: ${JSON.stringify(co
     scene.add(circle(colors[1], 1 - DX, 0.5, RADIUS))
     scene.add(circle(colors[2], 1 - 2 * DX, 0.5, RADIUS))
 
-    scene.add(circle(colors[2], -1, -0.5, RADIUS))
-    scene.add(circle(colors[1], -1 + DX, -0.5, RADIUS))
-    scene.add(circle(colors[2], -1 + 2 * DX, -0.5, RADIUS))
+    scene.add(circle(colors[1], -1, -0.5, RADIUS))
+    scene.add(circle(colors[2], -1 + DX, -0.5, RADIUS))
+    scene.add(circle(colors[1], -1 + 2 * DX, -0.5, RADIUS))
   }
 }
