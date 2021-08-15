f92c75ace3ecee0addd1ed362fbd631d832aa78c
Boris Panchenko <oberon@list.ru>
2021-08-15 21:16:10 +0300

    definition of the `onAndRun` method

--- src/js/lib/event-emmiter.js
+++ src/js/lib/event-emmiter.js
@@ -1,41 +1,54 @@
 const BUS = new Map
 
+function decomposeArguments(args) {
+    let obj, name, callback
+    
+    if (
+        args.length === 2 &&
+        typeof args[0] === 'string' &&
+        typeof args[1] === 'function'
+    ) {
+        obj = this
+        name = args[0]
+        callback = args[1]
+
+    } else if (
+        args.length === 3 &&
+        typeof args[0] === 'object' &&
+        typeof args[1] === 'string' &&
+        typeof args[2] === 'function'
+    ) {
+        obj = args[0]
+        name = args[1]
+        callback = args[2]
+        
+    } else console.error('Wrong Parameters')
+
+    console.log(obj, name, callback)
+
+    return { obj, name, callback }
+}
+
 export default class EventEmmiter {
 
     on(...args) {
-        let obj, name, callback
-        
-        if (
-            args.length === 2 &&
-            typeof args[0] === 'string' &&
-            typeof args[1] === 'function'
-        ) {
-            obj = this
-            name = args[0]
-            callback = args[1]
-
-        } else if (
-            args.length === 3 &&
-            typeof args[0] === 'object' &&
-            typeof args[1] === 'string' &&
-            typeof args[2] === 'function'
-        ) {
-            obj = args[0]
-            name = args[1]
-            callback = args[2]
-            
-        } else console.error('Wrong Parameters')
+        let { obj, name, callback } = decomposeArguments.call(this, args)
 
         let events = BUS.get(obj)
-
         if (!events) BUS.set(obj, events = new Map)
 
         let handlers = events.get(name)
-
         if (!handlers) events.set(name, handlers = new Set)
 
         handlers.add(callback.bind(obj))
 
+        return this
+    }
+
+    onAndRun(...args) {
+        this.on(...args)
+        let { obj, callback } = decomposeArguments(args)
+        callback.call(obj)
         return this
     }
 
@@ -73,8 +86,8 @@ export default class EventEmmiter {
     }
 
     static extend(obj) {
-        const { on, off, trigger } = EventEmmiter.prototype
-        Object.assign(obj, { on, off, trigger })
+        const { on, onAndRun, off, trigger } = EventEmmiter.prototype
+        Object.assign(obj, { on, onAndRun, off, trigger })
         return obj
     }
 }
