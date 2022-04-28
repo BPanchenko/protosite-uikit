1d8611d5fba205c83bb9a96266302a4d139caea5
Boris Panchenko <oberon@list.ru>
2022-04-29 02:21:33 +0300

    add package `postcss-color-function`

--- .config/postcss.config.js
+++ .config/postcss.config.js
@@ -4,8 +4,8 @@ module.exports = {
         require("cssnano"),
         require("postcss-import"),
         require("postcss-nested"),
-        require("postcss-custom-media"),
         require("postcss-custom-properties"),
-        require("postcss-color-mod-function")
+        require("postcss-color-function"),
+        require("postcss-custom-media")
     ]
 }
