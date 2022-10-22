138f54efc78f8764a650dee0f927f51b4ad11bd4
Boris Panchenko <oberon@list.ru>
2022-10-22 12:36:15 +0300

    extended build system

--- .config/cssnano.config.cjs
+++
@@ -1,7 +0,0 @@
-const defaultPreset = require('cssnano-preset-default')
-
-module.exports = defaultPreset({
-    discardComments: {
-        removeAll: true,
-    }
-})
--- .config/postcss.config.cjs
+++ .config/postcss.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
-    parser: false,
     plugins: [
-        require("cssnano")(require("./cssnano.config.cjs")),
+        require("cssnano"),
         require("postcss-import"),
         require("postcss-nested"),
         require("postcss-custom-properties"),
