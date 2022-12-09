1846898484f0d2a70886654ee4d78aabee5861ae
Boris Panchenko <oberon@list.ru>
2022-12-09 14:41:56 +0300

    replacing package `postcss-color-function` instead of `@csstools/postcss-color-function`

--- .config/postcss.config.cjs
+++ .config/postcss.config.cjs
@@ -4,8 +4,8 @@ module.exports = {
         require("cssnano"),
         require("postcss-import"),
         require("postcss-nested"),
+        require("postcss-color-function"),
         require("postcss-custom-media"),
-        require("@csstools/custom-units"),
-        require("@csstools/postcss-color-function")
+        require("@csstools/custom-units")
     ]
 }
