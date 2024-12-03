27a334a1532c314cdd56f435483f445d5eebb0de
Boris Vasilievich Panchenko <oberon@list.ru>
2024-12-03 17:12:24 +0300

    Fix: `CPanel` context

---
+++ assets/component/select-field.cjs
@@ -0,0 +1,22 @@
+const cssClassNames = {
+	"cPanel": "c-panel"
+};
+
+/** @type {CSSStyleSheet|null} */
+const cssStyleSheet = null;
+
+const path = require('node:path');
+const fs = require('node:fs');
+const file = path.join(__dirname, 'select-field.css');
+
+/** @type {string|null} */
+const cssText = fs.readFileSync(file, 'utf-8');
+
+module.exports = {
+	__esModule: true,
+	default: cssText,
+	cssStyleSheet,
+	cssText,
+	...cssClassNames
+
+}
---
+++ assets/component/select-field.mjs
@@ -0,0 +1,12 @@
+import importedCSS from './select-field.css' with { type: 'css' }
+
+/** @type {string} */
+export const cPanel = 'c-panel';
+
+/** @type {CSSStyleSheet|null} */
+export const cssStyleSheet = importedCSS;
+/** @type {string|null} */
+export const cssText = null;
+
+/** @type {CSSStyleSheet} */
+export default cssStyleSheet;
