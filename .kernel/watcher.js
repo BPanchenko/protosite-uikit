df9c0967545d0d316d6a64474474916666c9d1a9
Boris Vasilievich Panchenko <oberon@list.ru>
2025-01-22 12:40:02 +0300

    Remove: Assets watching

--- .kernel/watcher.js
+++ .kernel/watcher.js
@@ -6,22 +6,11 @@ import debounce from 'lodash/debounce.js'
 import Watcher from 'watcher'
 import { logger } from './logger.cjs'
 
-const build = debounce(() => child_process.exec('npm run build:css', (error, stdout, stderr) => logger.log(stdout)), 850, { maxWait: 1000 })
-const deploy = debounce(() => child_process.exec('npm run deploy', (error, stdout, stderr) => logger.log(stdout)), 2000, { maxWait: 3000 })
+const build = debounce(() => child_process.exec('npm run build', (error, stdout, stderr) => logger.log(stdout)), 240, { maxWait: 300 })
 
 const eventHandler = (event, targetPath, targetPathNext) => {
 	logger.event(event, targetPath, targetPathNext ?? '')
 }
-
-const watcherAssets = new Watcher(
-	['assets'].map((dir) => path.resolve(dir)),
-	{
-		recursive: true
-	},
-	eventHandler
-)
-	.on('ready', () => logger.info(`Watching Assets is running`))
-	.on('change', () => deploy())
 
 const watcherSources = new Watcher(
 	['component', 'document', 'object', 'settings', 'shadow-host', 'style', 'theme', 'utility'].map((dir) => path.resolve(dir)),
@@ -33,4 +22,4 @@ const watcherSources = new Watcher(
 )
 	.on('ready', () => logger.info(`Watching Source Code is running`))
 	.on('change', () => build())
-;['SIGINT', 'SIGTERM', 'SIGQUIT'].forEach((sig) => process.on(sig, () => (watcherAssets.close(), watcherSources.close())))
+;['SIGINT', 'SIGTERM', 'SIGQUIT'].forEach((sig) => process.on(sig, () => watcherSources.close()))
