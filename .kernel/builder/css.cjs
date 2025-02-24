6b09b06ea948815c58325a59e3f37ba3a01cd42c
Boris Vasilievich Panchenko <oberon@list.ru>
2025-02-24 12:11:59 +0300

    Opt: Remove sorces from the npm package

--- .kernel/builder/css.cjs
+++ .kernel/builder/css.cjs
@@ -11,35 +11,19 @@ const packageJson = require('../../package.json');
 const { parser, initConfig } = require('#config/postcss.config.cjs');
 
 const ROOT = process.cwd();
-const ADVANCED_FOLDERS = ['component', 'element', 'shadow-dom', 'style'];
 const DIST = path.resolve(ROOT, 'assets');
 
-// Input
-
-const patterns = (() => {
-  const packageJson = JSON.parse(readFileSync(path.resolve(ROOT, 'package.json'), { flag: 'r' }));
-
-  const regex1 = /^([-\w]+)\/\*$/i;
-  const regex2 = /[^-\w]+/i;
-
-  return packageJson.files
-    .filter((row) => regex1.test(row))
-    .map((row) => {
-      let pattern = row.replace(regex2, '');
-      if (ADVANCED_FOLDERS.includes(pattern)) {
-        pattern += '/*.css';
-      } else {
-        pattern += '/main.css';
-      }
-      return pattern;
-    });
-})();
-
 const files = glob
-  .sync(`{${patterns.join(',')},main.css}`, {
-    dot: false,
-    ignore: ['assets/**', 'settings/**']
-  })
+  .sync([
+    'component/*.css',
+    'element/*.css',
+    'object/main.css',
+    'shadow-dom/*.css',
+    'style/*.css',
+    'theme/main.css',
+    'utility/main.css',
+    'main.css'
+  ])
   .map((file) => path.resolve(ROOT, file));
 
 // Processing
@@ -47,9 +31,8 @@ const files = glob
 const promises = files.map((file) => {
   const { absTargetPath, folder } = getTargetFile(file);
 
-  const relTargetPath = path.relative(ROOT, absTargetPath);
+  const relTargetPath = path.relative(ROOT, absTargetPath).split(path.sep).join(path.posix.sep);
 
-  const module = path.join(packageJson.name, relTargetPath).split(path.sep).join(path.posix.sep);
   const rawCss = readFileSync(file, { flag: 'r' });
   const isShadyCSS = folder === 'shadow-dom';
   const isScopedModule = false === isEmpty(folder);
@@ -64,7 +47,7 @@ const promises = files.map((file) => {
     .process(rawCss, { from: file, to: absTargetPath, parser })
     .then((result) => {
       writeFileSync(absTargetPath, result.css, { flag: 'w' });
-      logger.logSuccess(module);
+      logger.logSuccess(relTargetPath);
     })
     .catch((error) => {
       logger.error(error);
